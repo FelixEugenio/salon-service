354587e1e32bfb9b834acf4e58a32827dd938f08
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-07-05 20:20:04 +0100

    feat: create getSalonById method

--- src/main/java/com/felix/controller/SalonController.java
+++ src/main/java/com/felix/controller/SalonController.java
@@ -1,4 +1,63 @@
 package com.felix.controller;
 
+import com.felix.mapper.SalonMapper;
+import com.felix.model.Salon;
+import com.felix.payload.dto.SalonDTO;
+import com.felix.payload.dto.UserDTO;
+import com.felix.service.SalonService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/salons")
+@RequiredArgsConstructor
 public class SalonController {
+
+    private final SalonService salonService;
+
+    @PostMapping
+    public ResponseEntity<SalonDTO> createSalon(@RequestBody @Valid SalonDTO salonDTO) throws Exception{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        Salon salon = salonService.createSalon(salonDTO,userDTO);
+        SalonDTO salonDTO1 = SalonMapper.mapToDTO(salon);
+        return ResponseEntity.ok(salonDTO1);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SalonDTO> updateSalon(@PathVariable("id") Long salonId, @RequestBody @Valid SalonDTO salonDTO) throws Exception{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        Salon salon = salonService.updateSalon(salonDTO,userDTO,salonId);
+        SalonDTO salonDTO1 = SalonMapper.mapToDTO(salon);
+        return ResponseEntity.ok(salonDTO1);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<SalonDTO>> getsalons() throws Exception{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        List<Salon> salons = salonService.getAllSalons();
+
+        List<SalonDTO> salonDTOS = salons.stream().map((salon) -> {
+            SalonDTO salonDTO = SalonMapper.mapToDTO(salon);
+            return salonDTO;
+        }).toList();
+       return ResponseEntity.ok(salonDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SalonDTO> getsalonById(@PathVariable("id") Long id) throws Exception{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        Salon salon = salonService.getSalonById(id);
+        SalonDTO salonDTO = SalonMapper.mapToDTO(salon);
+        return ResponseEntity.ok(salonDTO);
+    }
+
+
 }
---
+++ src/main/java/com/felix/mapper/SalonMapper.java
@@ -0,0 +1,22 @@
+package com.felix.mapper;
+
+import com.felix.model.Salon;
+import com.felix.payload.dto.SalonDTO;
+
+public class SalonMapper {
+
+    public static SalonDTO mapToDTO(Salon salon){
+        SalonDTO salonDTO = new SalonDTO();
+        salonDTO.setId(salon.getId());
+        salonDTO.setName(salon.getName());
+        salonDTO.setImages(salon.getImages());
+        salonDTO.setAddress(salon.getAddress());
+        salonDTO.setPhoneNumber(salon.getPhoneNumber());
+        salonDTO.setCity(salon.getCity());
+        salonDTO.setOwnerId(salon.getOwnerId());
+        salonDTO.setOpenTime(salon.getOpenTime());
+        salonDTO.setCloseTime(salon.getCloseTime());
+
+        return salonDTO;
+    }
+}
